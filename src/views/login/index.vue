2798c06d271b2804c0e319aff993eecb2f3f0a6b
Niko-Neko <875879153@qq.com>
2023-04-15 17:47:38 +0800

    完成了所有的登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,22 +6,31 @@
         </div>
         <center class="dec">欢迎你到来</center>
     </div>
-    <div class="main ">
+    <div class="main">
         <div class="form">
-                <input type="text">
-                <input type="text">
+            <div class="user">
+                <input type="text" placeholder="用户名">
+                <img src="../../assets/img/md-person Copy@2x.png" alt="">          
+                </div>
+                
+            <div  class="passwords">
+                <input type="text" placeholder="密码">
+                <img src="../../assets/img/md-lock Copy@2x.png" alt="">
+            </div>
                 <div class="operate">
                     <span>手机验证码登录</span>
                     <span>忘记密码</span>
                 </div>
                 <div class="login_reg">
-                    <van-button type="primary" round>登录</van-button>
-                    <van-button type="danger" round>注册</van-button>
+                    <div class="loginbtn">登录</div>
+                    <div class="regbtn">注册</div>
                 </div>
                 <div class="dsf">
                     <div class="dec">第三方登录</div>
                         <div class="decicon">
-                            
+                            <div><img src="../../assets/img/fab fa-qq Copy@2x.png" alt=""></div>
+                            <div><img src="../../assets/img/antOutline-wechat Copy@2x (1).png" alt=""></div>
+                            <div><img src="../../assets/img/fab fa-weibo Copy@2x (1).png" alt=""></div>
                         </div>
                 </div>
         </div>
@@ -62,8 +71,20 @@ export default {
             flex-direction: column;
             justify-content: center;
             gap: 18px;
-            input{
-                height: 45px;
+            .user,.passwords{
+                position: relative;
+                img{
+                    position: absolute;
+                    left: 18px;
+                    top: 50%;
+                    width: 24px;
+                    height: 24px;
+                    transform: translateY(-50%);
+                }
+            }
+               input{
+                padding-left:56px;
+                height: 42px;
                 border-radius: 5px 5px 5px 5px;
                 width: 100%;
                 border: 1px solid rgba(187, 187, 187, 1);
@@ -77,8 +98,35 @@ export default {
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
+                div{
+                    width: 130px;
+                    color:white;
+                    height: 38px;
+                    text-align: center;
+                    line-height: 38px;
+                    border-radius:6px ;
+                }
+                .loginbtn{
+                    background-color: rgba(189, 192, 200, 1);
+                }
+                .regbtn{
+                    background-color: rgba(113, 148, 255, 1);
+                }
             }
-            .dsf{}
+            .dsf{
+                .decicon{
+                    div{
+                        width: 24px;
+                        height: 24px;
+                        img{
+                            width: 100%;
+                        }
+                    }
+                    padding: 21px 24px;
+                    display: flex;
+                    justify-content: space-between;
+                }
+            }
         }
     }
     .foot{}
